d6d2ffe9e4fa70c12cf7b48eb97e16fe1ddd23c0
Arghya <argbhattacha@cs.stonybrook.edu>
2020-01-17 11:08:02 -0500

    update

---
+++ cache_ex.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+g++ binary-merge-sort.cpp -o ./executables/binary-merge-sort
+sudo chmod a+x ./executables/binary-merge-sort
+g++ k-way-merge-sort.cpp -o ./executables/k-way-merge-sort
+sudo chmod a+x ./executables/k-way-merge-sort
+
+
