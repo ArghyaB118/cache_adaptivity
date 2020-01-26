3bcd46709f88b899480b077712ee17d028cf92cb
Arghya <argbhattacha@cs.stonybrook.edu>
2020-01-26 16:08:40 -0500

    matrix init

--- cache_ex.sh
+++ cache_ex.sh
@@ -7,6 +7,11 @@ g++ k-way-merge-sort-cgroup.cpp -o ./executables/k-way-merge-sort-cgroup
 sudo chmod a+x ./executables/k-way-merge-sort-cgroup
 g++ funnel_sort.cpp -o ./executables/funnel_sort
 sudo chmod a+x ./executables/funnel_sort
+g++ cache_adaptive.cpp -o ./executables/cache-adaptive
+sudo chmod a+x ./executables/cache-adaptive
+g++ non_cache_adaptive.cpp -o ./executables/non-cache-adaptive
+sudo chmod a+x ./executables/non-cache-adaptive
+
 
 if [ -d  "/var/cgroups/$1" ]
 then
